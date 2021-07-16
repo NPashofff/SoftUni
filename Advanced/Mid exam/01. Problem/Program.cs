67d58d38e031251a49517bbd5b02e58485bc98fc
NPashofff <77660085+NPashofff@users.noreply.github.com>
2021-07-16 11:31:18 +0300

    Add files via upload

---
+++ Advanced/Mid exam/01. Problem/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01._Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double journeyCost = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+
+            double saevdMoneyMonthly = journeyCost * 0.25;
+            double saevdMoney = 0;
+
+            for (int i = 1; i <= months; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    saevdMoney += saevdMoney * 0.25;
+                    saevdMoney += saevdMoneyMonthly;
+                }
+                else if (i % 2 == 0 || i == 1)
+                {
+                    saevdMoney += saevdMoneyMonthly;
+                }
+                else if (i % 2 != 0)
+                {
+                    saevdMoney -= saevdMoney * 0.16;
+                    saevdMoney += saevdMoneyMonthly;
+                }
+            }
+            
+
+            if (saevdMoney >= journeyCost)
+            {
+                Console.WriteLine($"Bravo! You can go to Disneyland and you will have {saevdMoney - journeyCost:f2}lv. for souvenirs.");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry. You need {Math.Abs(saevdMoney - journeyCost):f2}lv. more.");
+            }
+            
+        }
+    }
+}
---
+++ Advanced/Mid exam/02. Problem/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cookies = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                if (input[0] == "Eat")
+                {
+                    break;
+                }
+                else if (input[0] == "Update-Any")
+                {
+                    for (int i = 0; i < cookies.Count; i++)
+                    {
+                        if (cookies[i] == input[1])
+                        {
+                            cookies[i] = "Out of stock";
+                        }
+                    }
+                }
+                else if (input[0] == "Remove")
+                {
+                    int idx = int.Parse(input[2]);
+                    if (idx > -1 && idx < cookies.Count)
+                    {
+                        cookies[idx] = input[1];
+                    }
+                }
+                else if (input[0] == "Update-Last")
+                {
+                    int lastCookies = cookies.Count - 1;
+                    cookies[lastCookies] = input[1];
+                }
+                else if (input[0] == "Rearrange")
+                {
+                    for (int i = 0; i < cookies.Count; i++)
+                    {
+                        if (cookies[i].Contains(input[1]))
+                        {
+                            cookies.RemoveAt(i);
+                            cookies.Add(input[1]);
+                        }
+                    }
+                }
+
+            }
+
+            foreach (var VARIABLE in cookies)
+            {
+                if (VARIABLE != "Out of stock")
+                {
+                    Console.Write(VARIABLE + " ");
+                }
+            }
+        }
+    }
+}
