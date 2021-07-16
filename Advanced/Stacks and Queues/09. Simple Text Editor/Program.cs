367bce5c66cb56ff4b385274980f6747009eb017
NPashofff <77660085+NPashofff@users.noreply.github.com>
2021-07-16 11:58:03 +0300

    Add files via upload

---
+++ Advanced/Stacks and Queues/09. Simple Text Editor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<string> undo = new Stack<string>();
+            string word = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", 2);
+                string command = input[0];
+
+                if (command == "1")
+                {
+                    undo.Push(word);
+                    word += input[1];  
+                }
+                else if (command == "2")
+                {
+                    int idx = int.Parse(input[1]);
+                    undo.Push(word);
+                    word = word.Remove(word.Length - idx);
+
+                }
+                else if (command == "3")
+                {
+                    int idx = int.Parse(input[1]);
+                    Console.WriteLine(word[idx - 1]);
+                }
+                else if (command == "4")
+                {
+                    word = undo.Pop();
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/Stacks and Queues/Truck Tour/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int[]> pumps = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                pumps.Enqueue(input);
+            }
+
+            int idx = 0;
+
+            while (true)
+            {
+                int totalFuel = 0;
+
+                foreach (int[] item in pumps)
+                {
+                    int fuel = item[0];
+                    int distance = item[1];
+
+                    totalFuel += fuel - distance;
+
+                    if (totalFuel < 0)
+                    {
+                        pumps.Enqueue(pumps.Dequeue());
+                        idx++;
+                        break;
+                    }
+                }
+
+                if (totalFuel >=0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(idx);
+        }
+    }
+}
