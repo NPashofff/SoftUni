d54d3981ce043099d6f57cbbdc5b7f6c4a8d4f98
NPashofff <77660085+NPashofff@users.noreply.github.com>
2021-07-16 12:11:50 +0300

    Add files via upload

---
+++ Advanced/Mid exam/Garden/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int row = dimensions[0];
+            int col = dimensions[1];
+            int[,] matrix = new int[row, col];
+            FillMatrix(row, col, matrix);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Bloom Bloom Plow")
+                {
+                    break;
+                }
+
+                int[] rowCol = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int rowP = rowCol[0];
+                int colP = rowCol[1];
+
+                if (IsIn(row, col, rowP, colP))
+                {                    
+                    for (int i = 0; i < row; i++)
+                    {
+                        if (i == rowP)
+                        {
+                            continue;
+                        }
+                            matrix[i, colP]++;                        
+                    }
+                    for (int i = 0; i < col; i++)
+                    {
+                        matrix[rowP, i]++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsIn(int row, int col, int rowP, int colP)
+        {
+            return rowP >= 0 && rowP < row && colP >= 0 && colP < col;
+        }
+
+        private static void FillMatrix(int row, int col, int[,] matrix)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/Mid exam/Scheduling/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tascks = new Stack<int>(
+                Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            
+            Stack<int> threads = new Stack<int>
+                (Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray());
+
+            int valueOfTasck = int.Parse(Console.ReadLine());
+            int tasck;
+            int thred;
+
+            while (true)
+            {
+                int tasckCurrent = tascks.Peek();
+                int thredCurrent = threads.Peek();
+
+                if (tasckCurrent == valueOfTasck)
+                {
+                    tasck = tascks.Pop();
+                    thred = threads.Pop();
+                    break;
+                }
+                else if (thredCurrent >= tasckCurrent)
+                {
+                    tasck = tascks.Pop();
+                    thred = threads.Pop();
+                }
+                else
+                {
+                    while (threads.Count > 0)
+                    {
+                        if (tasckCurrent > thredCurrent)
+                        {
+                            threads.Pop();
+                        }
+                        else
+                        {
+                            tasck = tascks.Pop();
+                            thred = threads.Pop();
+                            break;
+                        }
+
+                        thredCurrent = threads.Peek();
+                    }
+                }
+            }
+
+            threads.Push(thred);
+            Console.WriteLine($"Thread with value {thred} killed task {valueOfTasck}");
+            Console.WriteLine(String.Join(" ", threads));
+        }
+    }
+}
