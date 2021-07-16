9f11f383dccd1a8bf396a814b410b2891c764eca
NPashofff <77660085+NPashofff@users.noreply.github.com>
2021-07-16 11:45:04 +0300

    Add files via upload

---
+++ OOP/Inheritance/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,18 @@
+﻿namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public string Username { get; set; }
+        public int Level { get; set; }
+
+        public Hero(string name, int level)
+        {
+            Username = name;
+            Level = level;
+        }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {Username} Level: {Level}";
+        }
+    }
+}
