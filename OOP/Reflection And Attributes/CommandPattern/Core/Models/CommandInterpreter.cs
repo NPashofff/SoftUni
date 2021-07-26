b6653aef5d36e022af292cfd9e669f0513383eb5
NPashofff <77660085+NPashofff@users.noreply.github.com>
2021-07-26 14:35:33 +0300

    Add files via upload

---
+++ OOP/Reflection And Attributes/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,28 @@
+﻿namespace CommandPattern.Core.Models
+{
+    using Commands;
+    using Core.Contracts;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class CommandInterpreter : ICommandInterpreter
+
+    {
+        public string Read(string args)
+        {
+            string[] tockens = args.Split();
+            string commandName = tockens[0];
+            string[] commandArgs = tockens[1..];
+
+            Type commandType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == $"{commandName}Command");
+            ICommand command = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = command.Execute(commandArgs);
+            return result;
+        }
+    }
+}
---
+++ OOP/Reflection And Attributes/CommandPattern/Core/Models/Engine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Core.Models
+{
+    using Core.Contracts;
+    public class Engine : IEngine
+    {
+        private ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                string result = commandInterpreter.Read(command);
+
+                if (result == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
